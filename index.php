1c94d9814a29b734c5a199073f6d64084186feae
Marco Del Riego <marck94zac1@gmail.com>
2025-12-03 18:41:58 -0600

    feat: add header and footer structure to theme (classic approach)

--- index.php
+++ index.php
@@ -1,5 +1,12 @@
 <?php get_header(); ?>
 
-<?php the_content(); ?>
+<?php
+if (have_posts()) :
+    while (have_posts()) :
+        the_post();
+        the_content();
+    endwhile;
+endif;
+?>
 
 <?php get_footer(); ?>
